7adf10aca8a3a3d8111e2a35eb91317b7bef6ecd
sanmaopep <maoyiwei.ebay@bytedance.com>
2025-06-23 17:31:55 +0800

    fix: ts check

--- packages/materials/form-materials/src/components/batch-outputs/use-batch-outputs.tsx
+++
@@ -1,7 +0,0 @@
-import { FlowNodeEntity, useEntityFromContext } from '@flowgram.ai/editor';
-
-export function useBatchOutputVariables() {
-  const node: FlowNodeEntity = useEntityFromContext();
-
-  // listen to tree change which might cause change of children of loop
-}
