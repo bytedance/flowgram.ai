2385f5cc63999cb259c6f64c83abc46afeed174d
sanmaopep <maoyiwei.ebay@bytedance.com>
2025-09-11 17:14:43 +0800

    fix: type sequence

--- packages/variable-engine/json-schema/src/json-schema/json-schema-type-manager.tsx
+++ packages/variable-engine/json-schema/src/json-schema/json-schema-type-manager.tsx
@@ -51,8 +51,8 @@ export class JsonSchemaTypeManager<
     const registries = [
       defaultTypeDefinitionRegistry,
       stringRegistryCreator,
-      numberRegistryCreator,
       integerRegistryCreator,
+      numberRegistryCreator,
       booleanRegistryCreator,
       objectRegistryCreator,
       arrayRegistryCreator,
@@ -62,6 +62,7 @@ export class JsonSchemaTypeManager<
     ];
 
     registries.forEach((registry) => {
+      debugger;
       this.register(
         registry as unknown as JsonSchemaTypeRegistryCreator<
           Schema,
@@ -121,7 +122,7 @@ export class JsonSchemaTypeManager<
 
   public getDisplayIcon = (type: Schema) => {
     const registry = this.getTypeBySchema(type);
-    return registry?.getDisplayIcon(type) || registry?.icon || <></>;
+    return registry?.getDisplayIcon?.(type) || registry?.icon || <></>;
   };
 
   public getTypeSchemaProperties = (type: Schema) => {
